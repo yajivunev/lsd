3cdacb3737ad37b21f8f81cf10381a8f3dc85e51
yajivunev <64760651+yajivunev@users.noreply.github.com>
2022-08-05 20:24:23 -0500

    Update fragments.py
    make sob use only XY of lsds' mean offset in watershed_from_lsds

--- lsd/post/fragments.py
+++ lsd/post/fragments.py
@@ -34,9 +34,9 @@ def watershed_from_lsds(
     for z in range(depth):
         
         if mode == "canny":
-             sob = canny(denoise_tv_chambolle(lsds[0,z],weight=0.05),sigma=2) + canny(denoise_tv_chambolle(lsds[1,z],weight=0.05),sigma=2) + canny(denoise_tv_chambolle(lsds[2,z],weight=0.05),sigma=2)
+             sob = canny(denoise_tv_chambolle(lsds[1,z],weight=0.05),sigma=2) + canny(denoise_tv_chambolle(lsds[2,z],weight=0.05),sigma=2)
         elif mode == "sobel":
-             sob =  sobel(denoise_tv_chambolle(lsds[0,z],weight=0.05)) + sobel(denoise_tv_chambolle(lsds[1,z],weight=0.05)) + sobel(denoise_tv_chambolle(lsds[2,z],weight=0.05))
+             sob =  sobel(denoise_tv_chambolle(lsds[1,z],weight=0.05)) + sobel(denoise_tv_chambolle(lsds[2,z],weight=0.05))
         else: raise AssertionError("unknown watershed mode. choose 'canny' or 'sobel'.")
              
         thresh = threshold_otsu(sob)
